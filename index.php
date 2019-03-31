723881a768f3e473a2a2b58a070e8c859ca4ae00
bstafford2017 <b.stafford2017@gmail.com>
2019-03-31 13:21:31 -0500

    Version 1 -- 3/31/19

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>OURS</title>
+    </head>
+    <link rel="stylesheet" href="index.css" type="text/css"/>
+    <body>
+        <h3 id="welcome">Welcome to the Online University Registration System!</h3>
+        <form id="sign-in-form">
+            Username:<br>
+            <input type="text" id="username" required><br>
+            Password:<br>
+            <input type="text" id="password" required><br>
+            <input type="submit" id="submit" value="Submit" required><br>
+			<a href="sign-up.php">Do not have an account? Sign up today!</a>
+        </form>
+        <?php
+        
+        ?>
+    </body>
+</html>
---
+++ sign-up.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>OURS</title>
+    </head>
+    <link rel="stylesheet" href="sign-up.css" type="text/css"/>
+    <body>
+        <h3 id="welcome">Welcome to the Online University Registration System!</h3>
+        <form id="sign-up-form">
+            Username:<br>
+            <input type="text" id="username" required><br>
+            Password:<br>
+            <input type="text" id="password" required><br>
+			Confirm Password:<br>
+            <input type="text" id="confirm-password" required><br>
+            <input type="submit" id="submit" value="Create An Account" required><br>
+        </form>
+		<?php
+			
+		?>
+        </body>
+    </body>
+</html>
