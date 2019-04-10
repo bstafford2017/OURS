331419ddd6a139b570644db66aa4b66d2cebd4af
BrainLim <48869794+BrainLim@users.noreply.github.com>
2019-04-09 19:02:24 -0500

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -2,19 +2,17 @@
 <html>
     <head>
         <title>OURS</title>
-        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <link rel="stylesheet" href="css/sign-up.css" type="text/css"/>
     </head>
-    <body>
+    <body background= "http://www.cashadvance6online.com/data/archive/img/2871699048.jpeg">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js "></script>
       <script>
         $(document).ready(function(){
-
           $('#signup_form').on('submit', function(){
             $.ajax({
                  type: 'POST',
                  contentType: "application/json",
-
                  url: 'api/user/create.php',
                  data : JSON.stringify({
                    name : $('#name').val(),
@@ -27,32 +25,41 @@
                    // successfully sing up
                    alert("successfully created");
                  }
-
              }).fail(function(data) {
-
                  alert( "failed.");
              });
              // to prevent refreshing the whole page page
              return false;
-
           });
         });
-
       </script>
 
-        <h3>Welcome to the Online University Registration System!</h3>
-        <form id="signup_form">
-            Name:<br>
-            <input type="text" id="name" required><br>
-            Email:<br>
-            <input type="text" id="email" required><br>
-            Password:<br>
-            <input type="text" id="password" required><br>
-			      Confirm Password:<br>
-            <input type="text" id="confirm-password" required><br>
+        <br><br><center><h2><big><font color="white">Welcome to the Online University Registration System!</font></big></h2></br></br>
+          <form id="signup_form">
+            <div style="background-color:lightblue; height: 450px; width: 450px; border: solid black;">
+              <center><h1>Register</h1>
+              <center><p><small><b>Fill up the following details to register.</b></small>
+              <hr/>
+              <center>Name:<br>
+              <input type="text" placeholder="Enter Name" id="name" required><br>
+              <center>Email:<br>
+              <input type="text" placeholder="Enter Email" id="email" required><br>
+              <center>Password:<br>
+              <input type="text" placeholder="Enter Password" id="password" required><br>
+			        <center>Confirm Password:<br>
+              <input type="text" placeholder="Enter Confirm Password" id="confirm-password" required><br><br>
+              <center>Status:
+              <select>
+                <option value="student">Student</option>
+                <option value="staff">Staff</option>
+                <option value="faculty">Faculty</option>
+              </select><br><br>
             <input type="submit" value="Create An Account"><br>
+          </div>
         </form>
+      </div>
     </body>
 
 
 </html>
+
