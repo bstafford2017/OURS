b4fbbb632592964a09800491dd623f11dc836b4a
bstafford2017 <b.stafford2017@gmail.com>
2019-04-11 16:20:01 -0500

    Updated register page with password checking and altered formating a little

--- sign-up.php
+++ sign-up.php
@@ -5,11 +5,28 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <link rel="stylesheet" href="css/sign-up.css" type="text/css"/>
     </head>
-    <body background= "http://www.cashadvance6online.com/data/archive/img/2871699048.jpeg">
+    <body background= "https://pkpt.uol.edu.pk/portals/0/uol-pakpattan/assets/images/slider/lahore-campus.jpg">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js "></script>
       <script>
         $(document).ready(function(){
           $('#signup_form').on('submit', function(){
+
+            // Check if passwords match
+            var password = $('#password').val();
+            var con_password = $('#confirm-password').val();
+            if(password != con_password){
+              alert("Passwords do not match.");
+              exit();
+            }
+            
+            // Check password length with Regular Expression
+            var pass_regex = /\w\w\w\w\w\w\w*/;
+            var result = pass_regex.exec(password);
+            if(result != password){
+              alert("Password must be at least 7 characters long.");
+              exit();
+            }
+            
             $.ajax({
                  type: 'POST',
                  contentType: "application/json",
@@ -23,10 +40,10 @@
              }).done(function(data){
                  if(data){
                    // successfully sing up
-                   alert("successfully created");
+                   alert("Account Successfully Created.");
                  }
              }).fail(function(data) {
-                 alert( "failed.");
+                 alert( "Account Creation Failed.");
              });
              // to prevent refreshing the whole page page
              return false;
@@ -34,30 +51,43 @@
         });
       </script>
 
-        <br><br><center><h2><big><font color="white">Welcome to the Online University Registration System!</font></big></h2></br></br>
+        <br><br><center><h2>Welcome to the Online University Registration System!</h2></br></br>
           <form id="signup_form">
-            <div style="background-color:lightblue; height: 450px; width: 450px; border: solid black;">
               <center><h1>Register</h1>
               <center><p><small><b>Fill up the following details to register.</b></small>
-              <hr/>
               <center>Name:<br>
               <input type="text" placeholder="Enter Name" id="name" required><br>
-              <center>Email:<br>
+              <center>Username:<br>
+              <input type="text" placeholder="Enter Username" id="username" required><br>
+              <!--<center>Email:<br>
               <input type="text" placeholder="Enter Email" id="email" required><br>
+              -->
               <center>Password:<br>
-              <input type="text" placeholder="Enter Password" id="password" required><br>
+              <input type="password" placeholder="Enter Password" id="password" required><br>
 			        <center>Confirm Password:<br>
-              <input type="text" placeholder="Enter Confirm Password" id="confirm-password" required><br><br>
+              <input type="password" placeholder="Enter Confirm Password" id="confirm-password" required><br><br>
               <center>Status:
               <select>
                 <option value="student">Student</option>
                 <option value="staff">Staff</option>
                 <option value="faculty">Faculty</option>
               </select><br><br>
-            <input type="submit" value="Create An Account"><br>
-          </div>
+              <input id="submit-button" type="submit" value="Create An Account">
+              <a href="index.php" style="text-decoration:none;">
+                <input type="button" value="Back" href="index.php" formnovali>
+              </a>
         </form>
-      </div>
+        <?php
+        // Check if current username is being used
+        if(isset($_POST['submit-butt'])){
+          $first = $_POST['username'];
+          $check = "SELECT * FROM users WHERE name = '$username'";
+          $result = mysqli_query($con, $check);
+          if($result->num_rows >= 0){
+            echo '<script type="text/javascript"> alert("Invalid Username.") </script>';
+          } 
+        }
+        ?>
     </body>
 
 
